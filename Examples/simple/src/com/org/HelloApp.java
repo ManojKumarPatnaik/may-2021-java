fcbb49fe392e89a5444a69fce6df0443e6cb7e6f
Kishor <kishor1124@gmail.com>
2021-05-01 21:43:29 +0530

    first java program

---
+++ Examples/simple/src/com/org/HelloApp.java
@@ -0,0 +1,10 @@
+package com.org;
+
+public class HelloApp {
+	public static void main(String[] args) {
+		System.out.println("Welcome to eclipse");
+		int x = 10;
+		System.out.println("x = "+x); System.out.println("x = "+x);
+		
+	}
+}
